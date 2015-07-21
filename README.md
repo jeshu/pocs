bc4330f14691bb00a2c29c4ea464b6ebe3381752
unknown <brijesh.kumar@NOICLT166.imfcorp.com>
2015-07-21 18:54:06 +0530

    Readme updated fix

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Browser Zoom
 
-#### Need to check for browser zoom
-To deduct the browser zoom in web browsers, getZoom method can be used from 
+  #### Need to check for browser zoom
+  To deduct the browser zoom in web browsers, getZoom method can be used from 
 
-./browserZoom/deductZoom.js
+  ./browserZoom/deductZoom.js
 
-Please note there is a jQuery dependency, and it can be removed by normal browser deduction methods.
+  Please note there is a jQuery dependency, and it can be removed by normal browser deduction methods.
--- browserZoom/deductZoom.js
+++ browserZoom/deductZoom.js
@@ -1,3 +1,17 @@
+/**
+ * Author : JeshuBrij
+ * Date : 21-JULY-2015
+ * The JavaScript code in this page is free software: 
+ * you can redistribute it and/or modify it under the 
+ * terms of the GNU General Public License (GNU GPL) 
+ * as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later
+ * version.  The code is distributed WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU GPL for more details.
+ **/
+
 // update on zoom change as its also give resize event.
 $(window).on("resize", function(evt) {
     $("#Logger").text("Zoom level | " + getZoom() +" %");
